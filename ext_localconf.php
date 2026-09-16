74bca9faa8eb80f36c7e5683c6aac1ef8371f10c
Timothy Elias <elias+git@mogic.com>
2026-09-16 18:55:07 +0200

    [TASK] Fix typo3v13 deprecations

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,1 @@
 <?php
-
-call_user_func(static function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-        "@import 'EXT:custom_dashboard_widgets/Configuration/TSconfig/User/options.tsconfig'"
-    );
-});
